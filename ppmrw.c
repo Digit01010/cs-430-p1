961891184f4f3fa72c46c7ad3f8e5d1c30178d54
Jeffrey Covington <digit01010@gmail.com>
2016-09-15 19:08:05 -0600

    Added basic header parsing

--- ppmrw.c
+++ ppmrw.c
@@ -1,8 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+typedef struct Pixel {
+  unsigned char red, green, blue;
+} Pixel;
+
+typedef struct Header {
+   unsigned char magicNumber;
+   unsigned int width, height, maxColor;
+} Header;
+
+Header parseHeader(FILE *);
 
 int main(int argc, char *argv[]) {
-  printf("Hello World!\n");
+  if (argc != 4) {
+    printf("Usage: ppmrw mn input output\n");
+    return(1);
+  }
+  
+  FILE* fh = fopen(argv[2], "r");
+  Header h = parseHeader(fh);
+  printf("%d %d %d %d \n", h.magicNumber, h.width, h.height, h.maxColor);
+  fclose(fh);
   return 0;
 }
 
+Header parseHeader(FILE *fh) {
+  Header header;
+  
+  if (fgetc(fh) != 'P') {
+    exit(1);
+  }
+  
+  fscanf(fh, "%d ", &header.magicNumber);
+  fscanf(fh, "%d ", &header.width);
+  fscanf(fh, "%d ", &header.height);
+  fscanf(fh, "%d ", &header.maxColor);
+   
+  return header;
+}
